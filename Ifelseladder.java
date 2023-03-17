fbba3e9f84737ffb424a4014c62b54c930f1bcd0
aishwaryazunje <127471123+aishwaryazunje@users.noreply.github.com>
2023-03-17 12:21:55 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,25 @@
+class Ifelseladder
+{
+public static void main(String args[])
+{
+ String name =" Aishwarya ";
+  
+if (name == " pooja ")
+{
+ System.out.println("name is pooja");
+}
+ 
+else if ( name == " nisha ")
+{
+  System.out.println(" name is nisha ");
+}
+else if ( name == " priya ")
+{
+  System.out.println(" name is priya ");
+}
+else 
+{
+System.out.println(name);
+}
+}
+}
---
+++ Simpleif.java
@@ -0,0 +1,18 @@
+class Simpleif
+{
+  public static void main(String args[])
+{
+  int x = 5;
+  int y = 3;
+ if ( x + y < 5)
+{
+ System.out.println("x + y is less than 10");
+}
+else 
+{
+ System.out.println("x + y is greater than 10");
+
+}
+}
+}
+
