dfcc5424ffe8faf2c89bc401c812c26bce47306a
aishwaryazunje <127471123+aishwaryazunje@users.noreply.github.com>
2023-05-08 10:17:53 +0530

    Add files via upload

---
+++ ArrayIndexOutOfBound.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class ArrayIndexOutOfBound {
+	public static void main(String[] args) {
+		int a[]= {2,5,4,8,9};
+		for(int i=0;i<=a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
